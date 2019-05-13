6460587b8749d990b70ae7afde01f28e95f0950c
SamDino <samuel.cas.96@gmail.com>
2019-05-13 12:14:13 -0500

    Update de todo

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,16 @@
 class Admin {
+
     constructor(name, id){
         this.name = name;
         this.id = id;
     }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
 }
