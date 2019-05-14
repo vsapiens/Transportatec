8a467e7bb472b8aff8436b45d4e6023d689c8662
SamDino <samuel.cas.96@gmail.com>
2019-05-14 14:27:53 -0500

    Updates

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+import { isNull } from "util";
+
 function logout() {
     firebase.auth().signOut().then(function () {
         window.location.pathname = '/login.html'
     }).catch(function (error) {
         window.alert("Error: " + error);
     });
+}
+
+function modalAction() {
+    var routes = ["Valle 1","Valle 2","San Nicolas","Puerta de Hierro","Cumbres","San Jeronimo","Lincoln","Contry Linda Vista"];
+    var schedules = ["8:00","13:00","21:00"];
+
+    var scheduleIndex = document.getElementById("schedule").value;
+    var routeIndex = document.getElementById("route").value;
+
+    window.alert("scheduleIndex");
+
+    /* if (isNaN(routeIndex) || isNull(scheduleIndex)){
+        window.alert("Please select a valid route and schedule.");
+    } */
 }
